1a0b7404622af1ea4e055300d18006a9fbbe1795
saunaik19 <saunaik19@gmail.com>
2021-04-15 02:11:18 +0530

    First commit

---
+++ src/test/java/com/saurabh/example/SpringSecurityPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saurabh.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
